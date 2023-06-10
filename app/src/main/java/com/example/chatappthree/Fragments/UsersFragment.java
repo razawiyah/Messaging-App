841ed3bc408aa76d4c0d2536abff2621b477c522
Rabiatul Adawiyah ZA <rabiatul.nraza@gmail.com>
2023-06-10 18:58:40 +0800

    tabbedlayout

---
+++ app/src/main/java/com/example/chatappthree/Fragments/UsersFragment.java
@@ -0,0 +1,20 @@
+package com.example.chatappthree.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chatappthree.R;
+public class UsersFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+}
