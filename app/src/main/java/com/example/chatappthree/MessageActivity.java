e0e0fa25a3b11867e339bb4086f8ac5fafc647df
Rabiatul Adawiyah ZA <rabiatul.nraza@gmail.com>
2023-06-10 20:23:57 +0800

    send message

--- app/src/main/java/com/example/chatappthree/MessageActivity.java
+++ app/src/main/java/com/example/chatappthree/MessageActivity.java
@@ -8,11 +8,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.chatappthree.modelclass.UserModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,35 +24,42 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
 
-    String id;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser fUser;
+    String id,userId;
     View toolbar;
     TextView username;
-    ImageView back,menu;
-    public SignOut dialog;
+    ImageView back;
     CircleImageView profile_image;
-
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    ImageButton send_btn;
+    EditText text_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        id = getIntent().getStringExtra("userId");
+        fUser = mAuth.getCurrentUser();
+        id = fUser.getUid();
+
+        userId = getIntent().getStringExtra("userId");
+
+        send_btn = findViewById(R.id.send_btn);
+        text_send = findViewById(R.id.text_send);
+
 
         toolbar = findViewById(R.id.toolbar);
         username = toolbar.findViewById(R.id.username);
         back = toolbar.findViewById(R.id.backBtn);
         profile_image = toolbar.findViewById(R.id.profile_image);
-
-        dialog = new SignOut(this);
-
-
-
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,8 +68,7 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
-        Query query = databaseReference.child("User").child(id);
-
+        Query query = databaseReference.child("User").child(userId);
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -80,6 +90,19 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString().trim();
+                if(!msg.equals("")){
+                    sendMessage(id,userId,msg);
+                }else {
+                    errorPopup();
+                }
+                text_send.setText("");
+            }
+        });
     }
 
     @Override
@@ -87,4 +110,28 @@ public class MessageActivity extends AppCompatActivity {
         startActivity(new Intent(MessageActivity.this, MainActivity.class));
         finish();
     }
+
+    private void sendMessage(String sender, String reciever, String message){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("reciever",reciever);
+        hashMap.put("message",message);
+
+        databaseReference.child("Chats").setValue(hashMap);
+    }
+
+    private void errorPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Cannot send empty text!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Redirect to the homepage
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
